48634d077099d9d7a19954b03e8d8bbae8b409c8
choisuyong44 <tlgmdtl1118@inu.ac.kr>
2024-11-16 03:21:50 +0900

    feat/#7-load chatting room

--- vue-project/src/components/chat/ChatListOpen.vue
+++ vue-project/src/components/chat/ChatListOpen.vue
@@ -1,11 +1,27 @@
 <template>
     <div>
         <h2>OpenChatting</h2>
+        <hr>
+        <template v-for="room in openChatRoomList" :key="room">
+            <div>
+                1
+            </div>
+        </template>
     </div>
 </template>
 
 <script setup>
+    import { useChatRoomStore } from '@/stores/chatRoom';
+    import { onMounted, ref } from 'vue';
 
+    const store = useChatRoomStore()
+
+    const openChatRoomList = ref([])
+    const fetchOpenChatRoomList = function(){
+        openChatRoomList.value = store.loadOpenChatRoomList();
+    }
+
+    onMounted(fetchOpenChatRoomList)
 </script>
 
 <style scoped>
--- vue-project/src/components/chat/ChatListPrivate.vue
+++ vue-project/src/components/chat/ChatListPrivate.vue
@@ -1,10 +1,27 @@
 <template>
     <div>
         <h2>PrivateChatting</h2>
+        <hr>
+        <template v-for="room in chatRoomList" :key="room">
+            <div>
+                {{ room }}
+            </div>
+        </template>
     </div>
 </template>
 
 <script setup>
+    import { useChatRoomStore } from '@/stores/chatRoom';
+import { onMounted, ref } from 'vue';
+
+    const store = useChatRoomStore()
+
+    const chatRoomList = ref([]);
+    const fetchChatRoomList = function(){
+        chatRoomList.value = store.loadChatRoomList();
+    }
+
+    onMounted(fetchChatRoomList)
 
 </script>
 
